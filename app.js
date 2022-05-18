bb7f1ede9758cf0d5b6432a124684e299215293a
nivtal9 <nivtal9@gmail.com>
2022-05-19 00:10:13 +0300

    routes and hbs
    apply routes and hbs layouts starting

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const app=express()
 const connectDB= require('./config/db')
+const exphbs = require('express-handlebars')
 const PORT =process.env.PORT||3000
 
+//environment configuration
 dotenv.config({path: './config/config.env'})
 
+const app=express()
+
 connectDB()
+
+// viewes and hbs
+app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout:`main`}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+//routes
+app.use('/', require ('./routes/index'))
 
 app.get('/', (req,res) =>{
     res.send('hello world')
