3ef7877520455b2a52480b30fe95e6ca4fc904b3
nivtal9 <nivtal9@gmail.com>
2022-05-19 01:46:03 +0300

    OAuth logging the profile
    not finalized OAuth, callback for login needed

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const path = require('path')
 const dotenv = require('dotenv')
 const connectDB= require('./config/db')
 const exphbs = require('express-handlebars')
-const PORT =process.env.PORT||3000
+const passport = require('passport')
+const session = require('express-session')
+const PORT = process.env.PORT||3000
+
+const app=express()
 
 //environment configuration
 dotenv.config({path: './config/config.env'})
 
-const app=express()
+//passport configuration 
+require('./config/passport')(passport);
 
 connectDB()
 
@@ -17,11 +22,23 @@ app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout:`main`}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+//sessions : https://www.npmjs.com/package/express-session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require ('./routes/index'))
+app.use('/auth', require ('./routes/auth'))
 
 app.get('/', (req,res) =>{
     res.send('hello world')
