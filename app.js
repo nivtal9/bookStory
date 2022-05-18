a42b6c15129689e0938be2ed8141d4ad9d033196
nivtal9 <nivtal9@gmail.com>
2022-05-18 22:57:35 +0300

    connected DB to application

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const app=express()
+const connectDB= require('./config/db')
 const PORT =process.env.PORT||3000
 
 dotenv.config({path: './config/config.env'})
+
+connectDB()
 
 app.get('/', (req,res) =>{
     res.send('hello world')
